f16f7231457cf275867fbfb45844e2efcb23f4f4
nollenr <nollenr@chopin.(none)>
2014-10-08 15:25:30 -0700

    Models changed to include has_many/one:belongs_to (including self join).  Creating notes on selection and configuration.  Testing those models and scopes to achieve the configuration.

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,2 +1,7 @@
 class Library < ActiveRecord::Base
+  has_one :library_file_spec, foreign_key: "idoflibraryrecord"
+  
+  # http://guides.rubyonrails.org/association_basics.html#self-joins
+  has_many :childrenl, class_name: "Library", foreign_key: "idofparent"
+  belongs_to :parentl, class_name: "Library", foreign_key: "idofparent"
 end
--- app/models/library_file_spec.rb
+++ app/models/library_file_spec.rb
@@ -1,2 +1,20 @@
 class LibraryFileSpec < ActiveRecord::Base
+  belongs_to :library, foreign_key: "idoflibraryrecord"
+  
+  def self.has_title
+    where.not(title: nil)
+  end
+  def self.title_not_like_track
+    # where("upper(title) not like 'TRACK%'")
+    where.not("upper(title) like 'TRACK%'")
+  end
+  def self.default_group_by
+    group(:title, :album, :artist)
+  end
+  def self.add_track_to_group_by
+    group(:track)
+  end
+  def self.with_file_name
+    select(:name).joins(:library)
+  end
 end
