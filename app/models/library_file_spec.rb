1212bffa2a32791d0db81925379cf461adba91a3
nollenr <nollenr@chopin.(none)>
2014-10-09 09:47:48 -0700

    Scopes are updated, complete and tested per the config items currently tested along with documentation in 'config items'.

--- app/models/library_file_spec.rb
+++ app/models/library_file_spec.rb
@@ -1,6 +1,9 @@
 class LibraryFileSpec < ActiveRecord::Base
   belongs_to :library, foreign_key: "idoflibraryrecord"
   
+  #############################################
+  # SCOPES                                    #
+  #############################################
   def self.has_title
     where.not(title: nil)
   end
@@ -8,13 +11,23 @@ class LibraryFileSpec < ActiveRecord::Base
     # where("upper(title) not like 'TRACK%'")
     where.not("upper(title) like 'TRACK%'")
   end
+  
+  # all of the following are to be used with a ".count"
   def self.default_group_by
     group(:title, :album, :artist)
   end
   def self.add_track_to_group_by
     group(:track)
   end
-  def self.with_file_name
-    select(:name).joins(:library)
+  
+  # The following two scopes are mutually exclusive
+  def self.with_filename
+    joins(:library).group("libraries.name")
+  end
+  def self.with_parent_dir
+    joins(library: :parentl).group("parentls_libraries.name")
+  end
+  def self.with_filename_and_parent_dir
+    joins(library: :parentl).group("libraries.name", "parentls_libraries.name")
   end
 end
